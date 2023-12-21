e8bb34722c5958a48011497df69f5d578373ea74
955928 <145359084+955928@users.noreply.github.com>
2023-12-21 21:11:36 +0100

    Exercice3 finished

--- exercice_3.py
+++ exercice_3.py
@@ -1,15 +1,20 @@
 import random
 
-def plus_grands_num(a, b):
-
-    list_dimension = (a, b)
+def plus_grands_num(a, b):    
+    list_size = (a, b)
     #creation de l'algo qui génère une liste de 100 entiers 
-    list_dimension = random.choices(range(a, b+1), k=100)
+    list_size = random.choices(range(a, b+1), k=100)
     #'+1' permet d'intégrer le dernier chiffre
-    return list_dimension
+    nombre_entiers_a_ex = sorted(list_size, reverse=True)
+    entiers_extraits = nombre_entiers_a_ex[:10]
+
+    print(list_size ),ad
+    print(entiers_extraits)
+    return
 
 
 result = plus_grands_num(0, 99)
+
 print(result)
 
 
@@ -26,3 +31,4 @@ print(result)
 
 # result = plus_grands_num(0, 99)
 # print(result)
+
